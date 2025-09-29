bddd1ca765f3122f3aa6c01ea203a7de5b0606a6
neryadom <henrichemla@outlook.com>
2025-09-30 00:02:45 +1300

    adding stub

---
+++ string_printer.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+char *string_printer(const char *input_string){
+	if (input_string ==NULL){
+		return NULL;
+	} else {
+		size_t n = strlen(input_string) + 1;
+		char *output = malloc(n);
+		if (!output) return NULL;
+		memcpy(output, input_string, n);
+		return output;
+	}
+}
+
+int main(int argc, char *argv[]){
+	if (argc > 2){
+		printf("Error - program takes only one positional argument\n");
+		return 1;
+	}
+	char *output = string_printer(argv[1]);
+	printf("%s\n", output);
+	free(output);
+	return 0;
+}
